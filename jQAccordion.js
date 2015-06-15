c8fab839519b47def02986b77247966bb1325970
Hendrikto <hendrik.to@gmail.com>
2015-06-15 17:38:32 +0200

    Initial commit. Contains first jQUnix module: jQAccordion.

---
+++ jQAccordion.js
@@ -0,0 +1,31 @@
+/***********************************************************
+
+jQAccordion.js
+v 0.1
+
+A jQuery plugin that creates an accordion style menu.
+
+***********************************************************/
+(function($) {
+	$.fn.accordion = function (options) {
+		options = options || {};
+		var	trigger = options.trigger || "div",
+			slider = options.slider || "ul",
+			event = options.event || "click",
+			duration = options.duration || 400,
+			node = this,
+			$trigger = $(trigger, this),
+			$slider = $(slider, this);
+
+		$trigger.each(function(i) {
+			var $thisSlider = $($slider[i]);
+			$(this).on(event, function () {
+				$thisSlider.add(".open", node)
+					.slideToggle(duration)
+					.toggleClass("open");
+			});
+		});
+		// make plugin chainable
+		return this;
+	};
+})(jQuery);
