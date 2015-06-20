9e23227ecd41748eb1f5eacf6110684d151af731
Hendrikto <hendrik.to@gmail.com>
2015-06-20 17:36:00 +0200

    Add new jQUnix module: jQDisplaceBody.

---
+++ jQDisplaceBody.js
@@ -0,0 +1,38 @@
+/***********************************************************
+
+jQDisplaceBody.js
+v0.1
+
+A jQuery plugin that creates a slide in effect which
+displaces the page content.
+
+***********************************************************/
+(function($) {
+	$.fn.displaceBody = function (trigger, options) {
+		// return if no jQuery object was provided as a trigger
+		if (!(trigger instanceof $)) {
+			return this;
+		}
+		options = options || {};
+		var event = options.event || "click",
+			position = options.position || "left",
+			node = this,
+			nodeWidth = this.outerWidth(),
+			nodeHidden = true,
+			$body = $("body");
+		trigger.on(event, function () {
+			if (nodeHidden) {
+				// open
+				$body.css(position, nodeWidth);
+				node.css(position, 0);
+			} else {
+				// close
+				$body.css(position, 0);
+				node.css(position, -nodeWidth);
+			}
+			nodeHidden = !nodeHidden;
+		});
+		// make plugin chainable
+		return this;
+	};
+})(jQuery);
